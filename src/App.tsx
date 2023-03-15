cece1fb986ddc438ba57f26adb1d4a94ef61689f
Clearenough <smogida1337@gmail.com>
2023-03-15 17:15:51 +0300

    feet: add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,25 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    <div className="wrapper">
+      <Routes>
+        <Route path="/" element={<>Hi</>} />
+        <Route path="/about" element={<>About Us</>} />
+        <Route path="/404" element={<>404</>} />
+        <Route path="*" element={<Navigate to={'/404'} />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
+
+export default WrappedApp;
