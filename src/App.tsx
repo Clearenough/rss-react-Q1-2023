bf42dd69ef6369d6ab0760675b9357ffdbba29e4
Clearenough <smogida1337@gmail.com>
2023-03-16 14:29:25 +0300

    feet: add NotFound page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MainPage from './pages/Main';
 import AboutUs from './pages/AboutUs';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/404" element={<>404</>} />
+        <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to={'/404'} />} />
       </Routes>
     </div>
