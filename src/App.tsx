27f5d2bcd8732329e9faf74093ace7773e0e460f
Clearenough <smogida1337@gmail.com>
2023-03-16 14:17:18 +0300

    feet: add main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.scss';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <Routes>
-        <Route path="/" element={<>Hi</>} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<>About Us</>} />
         <Route path="/404" element={<>404</>} />
         <Route path="*" element={<Navigate to={'/404'} />} />
