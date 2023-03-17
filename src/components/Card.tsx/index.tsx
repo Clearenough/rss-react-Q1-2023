3379cd07cf684f752b8b2012f9708f54d489d572
Clearenough <smogida1337@gmail.com>
2023-03-17 16:54:01 +0300

    feet: add card

---
+++ src/components/Card.tsx/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+interface IProps {
+  imageUrl: string;
+  name: string;
+  price: number;
+}
+
+interface IState {
+  count: number;
+}
+
+class Card extends React.Component<IProps, IState> {
+  state: IState = { count: 1 };
+
+  constructor(props: IProps) {
+    super(props);
+  }
+
+  increaseCount = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const count = this.state.count;
+    this.setState({ count: count + 1 });
+  };
+
+  decreaseCount = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const count = this.state.count;
+    if (count === 0) return;
+    this.setState({ count: count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="card">
+        <img src="" alt="" />
+        <div>
+          <span>{this.props.name} </span>
+          <span>total price: {this.state.count * this.props.price}</span>
+        </div>
+        <div>
+          <button onClick={this.decreaseCount}> - </button>
+          <span>{this.state.count}</span>
+          <button onClick={this.increaseCount}> + </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,13 @@
+import Card from './../components/Card.tsx';
 import SearchBar from './../components/SearchBar';
 
 function MainPage() {
-  return <SearchBar />;
+  return (
+    <>
+      <SearchBar />
+      <Card imageUrl="" name={'asd'} price={2} />
+    </>
+  );
 }
 
 export default MainPage;
