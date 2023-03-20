ccceb9f00418faeadb83eb9c6f5ee53c688e9cf5
Clearenough <smogida1337@gmail.com>
2023-03-20 19:28:53 +0300

    refactor: delete redundant comments, console logs and methods

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styles from './index.module.scss';
-// interface SearchBarProps {}
-
-// interface SearchBarState {}
 
 class SearchBar extends React.Component {
   state = { inputValue: '' };
 
   componentDidMount() {
     const inputValue = localStorage.getItem('INPUT_VALUE');
-    console.log(localStorage, 'mount');
     if (inputValue) {
       this.setState({ inputValue: inputValue });
     }
@@ -17,13 +13,6 @@ class SearchBar extends React.Component {
 
   componentWillUnmount() {
     const inputValue = this.state.inputValue;
-    localStorage.setItem('INPUT_VALUE', inputValue);
-    console.log(localStorage, 'unmount');
-  }
-
-  onInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const inputValue = event.target.value;
-    this.setState({ inputValue });
     localStorage.setItem('INPUT_VALUE', inputValue);
   }
 
