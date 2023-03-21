1597c19cba5590865f704d142278425d15f7b7cd
Clearenough <smogida1337@gmail.com>
2023-03-21 18:40:30 +0300

    feet: add form page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import MainPage from './pages/Main';
 import AboutUs from './pages/AboutUs';
 import NotFound from './pages/NotFound';
+import MyForm from './pages/Form';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/form" element={<MyForm />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to={'/404'} />} />
       </Routes>
---
+++ src/pages/Form.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// interface MyFormProps {}
+
+// interface MyFormState {}
+
+class MyForm extends React.Component {
+  // state = { :  }
+  render() {
+    return <>Form</>;
+  }
+}
+
+export default MyForm;
