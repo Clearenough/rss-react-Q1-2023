f048e752e76cab83b2efcbbef9ad3a6ff939073c
Clearenough <smogida1337@gmail.com>
2023-03-24 12:25:35 +0300

    style: add styles for Form component

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,6 +1,7 @@
 import { IFormCard } from './../@types/common';
 import React, { createRef, RefObject } from 'react';
 import FormCards from './../components/FormCards';
+import styles from './Form.module.scss';
 
 interface IMyFormState {
   isTextFilled: boolean;
@@ -11,9 +12,9 @@ interface IMyFormState {
 
 class MyForm extends React.Component<unknown, IMyFormState> {
   state: IMyFormState = {
-    isTextFilled: false,
-    isDateChoosed: false,
-    isImageChoosed: false,
+    isTextFilled: true,
+    isDateChoosed: true,
+    isImageChoosed: true,
     cards: [],
   };
 
@@ -26,7 +27,6 @@ class MyForm extends React.Component<unknown, IMyFormState> {
 
   onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(this.state);
     let image: File;
     const files = this.inputFileRef.current?.files;
     const inputText = this.inputTextRef.current?.value;
@@ -68,26 +68,85 @@ class MyForm extends React.Component<unknown, IMyFormState> {
   render() {
     return (
       <div>
-        <form onSubmit={this.onSubmit}>
-          <input type="text" name="text" ref={this.inputTextRef} />
-          <label htmlFor="text">Type something</label>
-          {!this.state.isTextFilled && <span>Error</span>}
-          <input type="date" name="date" ref={this.inputDateRef} />
-          <label htmlFor="date">pick a random date</label>
-          {!this.state.isDateChoosed && <span>Error</span>}
-          <select name="select" ref={this.selectRef}>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-          <label htmlFor="select">Yes or No, what would you choose?</label>
-          <input type="checkbox" name="checkbox" ref={this.inputCheckboxRef} />
-          <label htmlFor="checkbox">leave him alone he doesnt want to be disturbed</label>
-          <input type="radio" name="radio" ref={this.inputRadioRef} />
-          <label htmlFor="radio">turn on the radio</label>
-          <input type="file" name="file" accept="image/jpeg,image/png" ref={this.inputFileRef} />
-          <label htmlFor="file">pick photo</label>
-          {!this.state.isImageChoosed && <span>Error</span>}
-          <button type="submit">Create Card</button>
+        <form className={styles.form} onSubmit={this.onSubmit}>
+          <div className={styles.inputLayout}>
+            <label htmlFor="text">Type something: </label>
+            <input
+              className={styles.inputWithField}
+              type="text"
+              name="text"
+              id="text"
+              ref={this.inputTextRef}
+            />
+            {!this.state.isTextFilled && <span className={styles.error}>Error</span>}
+          </div>
+
+          <div className={styles.inputLayout}>
+            <label htmlFor="date">pick a random date</label>
+            <input
+              className={styles.inputWithField}
+              type="date"
+              name="date"
+              id="date"
+              ref={this.inputDateRef}
+            />
+
+            {!this.state.isDateChoosed && <span className={styles.error}>Error</span>}
+          </div>
+
+          <div className={styles.inputLayout}>
+            <label htmlFor="select">Yes or No, what would you choose?</label>
+            <select
+              className={styles.inputWithField}
+              name="select"
+              id="select"
+              ref={this.selectRef}
+            >
+              <option value="Yes">Yes</option>
+              <option value="No">No</option>
+            </select>
+          </div>
+
+          <div className={styles.inputLayout}>
+            <label htmlFor="checkbox">leave him alone he doesnt want to be disturbed</label>
+            <input
+              className={styles.clickableField}
+              type="checkbox"
+              name="checkbox"
+              id="checkbox"
+              ref={this.inputCheckboxRef}
+            />
+          </div>
+
+          <div className={styles.inputLayout}>
+            <label htmlFor="radio">turn on the radio</label>
+            <input
+              className={styles.clickableField}
+              type="radio"
+              name="radio"
+              id="radio"
+              ref={this.inputRadioRef}
+            />
+          </div>
+
+          <div className={styles.inputLayout}>
+            <label className={styles.photoLable} htmlFor="file">
+              pick photo
+            </label>
+            <input
+              className={styles.invisible}
+              type="file"
+              name="file"
+              id="file"
+              accept="image/jpeg,image/png"
+              ref={this.inputFileRef}
+            />
+            {!this.state.isImageChoosed && <span className={styles.error}>Error</span>}
+          </div>
+
+          <button className={styles.submit} type="submit">
+            Create Card
+          </button>
         </form>
         <FormCards cards={this.state.cards} />
       </div>
