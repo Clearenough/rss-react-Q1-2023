40158372b468db0b12de420d8a44077a5a937826
Clearenough <smogida1337@gmail.com>
2023-03-24 12:52:26 +0300

    style: add styles fo FormCards component

--- src/components/FormCards/index.tsx
+++ src/components/FormCards/index.tsx
@@ -1,6 +1,7 @@
 import { IFormCard } from './../../@types/common';
 import React from 'react';
 import FormCard from './../../components/FormCard';
+import styles from './index.module.scss';
 
 interface IProps {
   cards: IFormCard[];
@@ -9,7 +10,7 @@ interface IProps {
 class FormCards extends React.Component<IProps, unknown> {
   render() {
     return (
-      <div>
+      <div className={styles.cards}>
         {this.props.cards.map((card, index) => {
           return <FormCard {...card} key={index} />;
         })}
