90a3de6959083517902e78d7c163025e26f5ff5f
Clearenough <smogida1337@gmail.com>
2023-04-09 16:13:49 +0300

    refactor: delete redundant import and comment

--- src/components/CardsList/index.tsx
+++ src/components/CardsList/index.tsx
@@ -1,14 +1,6 @@
 import Card from './../Card.tsx';
-import cardsInfo from './../../assets/cardsInfo.json';
 import styles from './index.module.scss';
 import { ICharacterCard } from '../../@types/common';
-
-// interface ICard {
-//   imageUrl: string;
-//   name: string;
-//   price: number;
-//   id: number;
-// }
 
 interface IProps {
   cards: ICharacterCard[];
